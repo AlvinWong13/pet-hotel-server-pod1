7be2bf0d175c54b777eaf65dfc79670f9f834855
Vada Karlen <vada.karlen@gmail.com>
2021-03-31 14:41:31 -0500

    Python stuffs

---
+++ script.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, make_response
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    print('we made it to the server')
+    return "<h1>Hello Pet World!</h1><p>I Am in python flask</p>"
+
+@app.route('/owners', methods=['GET'])
+def get_owners():
+    connection = psycopg2.connect(user="", host="127.0.0.1", port="5432", database="pethotel")
+    cursor = connection.cursor(cursor_factory=RealDictCursor)
+    query_text = "SELECT owners.id, owners.name, COUNT(pets.name) AS count FROM owners JOIN pets ON owners.id = pets.owner_id GROUP BY owners.id, owners.name"
+    # execute query
+    cursor.execute(query_text)
+    #Select rows from table using cursor.fetchall
+    owners = cursor.fetchall()
+    print(owners)
+    #response 200
+    return jsonify(owners)
+
+app.run()
